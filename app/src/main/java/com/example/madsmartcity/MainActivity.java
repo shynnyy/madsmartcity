2eb4f024e366431103e2d1b9a5c751159cc1d644
yasuarin <dadiaputu@gmail.com>
2024-11-03 11:24:21 +0700

    loginVideo

--- app/src/main/java/com/example/madsmartcity/MainActivity.java
+++ app/src/main/java/com/example/madsmartcity/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.madsmartcity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.madsmartcity.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        VideoView intro = findViewById(R.id.intro);
+        intro.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.video);
+
+        MediaController mediaController = new MediaController(this);
+        intro.setMediaController(mediaController);
+        mediaController.setAnchorView(intro);
+        intro.start();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
